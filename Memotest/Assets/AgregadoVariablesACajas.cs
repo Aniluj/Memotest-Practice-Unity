9d6690949ad6ed1a4a46ed513b868726ea188a47
Aniluj <julianvolonterio@gmail.com>
2017-05-14 17:35:20 -0300

    Funcionalidad terminada
    Corrección y modificación de los scripts para lograr el funcionamiento
    correspondiente al memotest.

---
+++ Memotest/Assets/AgregadoVariablesACajas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgregadoVariablesACajas : MonoBehaviour {
+
+	public int variableDeCaja;
+	public ControladorDeCajas controlador;
+
+	void Start () {
+		
+	}
+		
+	void OnMouseDown(){
+		controlador.comparador (gameObject.GetComponent<AgregadoVariablesACajas>());
+	}
+}
---
+++ Memotest/Assets/ControladorDeCajas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorDeCajas : MonoBehaviour {
+
+	private AgregadoVariablesACajas caja1;
+	private AgregadoVariablesACajas caja2;
+	public int cont = 0;
+
+	public void comparador(AgregadoVariablesACajas caja)
+	{
+		cont++;
+
+		if (cont == 1) {
+			caja1 = caja;
+			} else {
+			caja2 = caja;
+			if (caja1.variableDeCaja == caja2.variableDeCaja) {
+				Destroy (caja1.gameObject);
+				Destroy (caja2.gameObject);
+			}
+			cont = 0;
+		}
+	}
+}
